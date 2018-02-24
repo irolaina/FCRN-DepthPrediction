bcaeb81e986107c13289c70eb2c9d729a577e796
nicolasrosa <nicolas.rosa@hotmail.com>
2018-02-24 17:13:46 -0300

    [MOD] Training Model working!

--- tensorflow/predict_nick.py
+++ tensorflow/predict_nick.py
@@ -30,7 +30,7 @@ appName = 'monodeep'
 datetime = time.strftime("%Y-%m-%d") + '_' + time.strftime("%H-%M-%S")
 
 ENABLE_EARLY_STOP = True
-SAVE_TRAINED_MODEL = False # TODO: mudar pra true antes de fazer uma simulacao
+SAVE_TRAINED_MODEL = True
 ENABLE_TENSORBOARD = True
 SAVE_TEST_DISPARITIES = True
 APPLY_BILINEAR_OUTPUT = False
@@ -714,6 +714,209 @@ def createSaveFolder():
 
     return save_path, save_restore_path
 
+class Plot(object):
+    def __init__(self, mode, title):
+        self.fig, self.axes = None, None
+
+        if mode == 'train':
+            self.fig, self.axes = plt.subplots(4, 1)
+            self.axes[0] = plt.subplot(221)
+            self.axes[1] = plt.subplot(223)
+            self.axes[2] = plt.subplot(222)
+            self.axes[3] = plt.subplot(224)
+
+        elif mode == 'test':
+            self.fig, self.axes = plt.subplots(5, 1)
+            self.axes[0] = plt.subplot(321)
+            self.axes[1] = plt.subplot(323)
+            self.axes[2] = plt.subplot(325)
+            self.axes[3] = plt.subplot(322)
+            self.axes[4] = plt.subplot(324)
+
+        self.fig.canvas.set_window_title(title)
+        self.isFirstTime = True
+
+    # TODO: Add ColorBar
+    def showTrainResults(self, raw, label, log_label, pred):
+        plt.figure(1)
+
+        # Set Titles and subplots spacing. Runs only at first Time
+        if self.isFirstTime:
+            self.axes[0].set_title("Raw")
+            self.axes[1].set_title("Label")
+            self.axes[2].set_title("log(Label)")
+            self.axes[3].set_title("Pred")
+            plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
+            self.isFirstTime = False
+
+        self.axes[0].imshow(raw)
+        cax1 = self.axes[1].imshow(label)
+        # self.fig.colorbar(cax1)
+        cax2 = self.axes[2].imshow(log_label)
+        # self.fig.colorbar(cax2)
+        cax3 = self.axes[3].imshow(pred)
+        # self.fig.colorbar(cax3)
+
+        plt.pause(0.001)
+
+    def showValidResults(self, raw, label, log_label, coarse, fine):
+        plt.figure(2)
+
+        # Set Titles and subplots spacing. Runs only at first Time
+        if self.isFirstTime:
+            self.axes[0].set_title("Raw")
+            self.axes[1].set_title("Label")
+            self.axes[2].set_title("log(Label)")
+            self.axes[3].set_title("Coarse")
+            self.axes[4].set_title("Fine")
+            plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
+            self.isFirstTime = False
+
+        self.axes[0].imshow(raw)
+        cax1 = self.axes[1].imshow(label)
+        # self.fig.colorbar(cax1)
+        cax2 = self.axes[2].imshow(log_label)
+        # self.fig.colorbar(cax2)
+        cax3 = self.axes[3].imshow(coarse)
+        # self.fig.colorbar(cax3)
+        cax4 = self.axes[4].imshow(fine)
+        # self.fig.colorbar(cax4)
+
+        plt.pause(0.001)
+
+
+    def showTestResults(self, raw, label, log_label,coarse, fine, i):
+        plt.figure(1)
+
+        # Set Titles and subplots spacing. Runs only at first Time
+        if self.isFirstTime:
+            self.axes[0].set_title("Raw")
+            self.axes[1].set_title("Label")
+            self.axes[2].set_title("log(Label)")
+            self.axes[3].set_title("Coarse")
+            self.axes[4].set_title("Fine")
+            plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+            self.isFirstTime = False
+
+        self.fig.canvas.set_window_title("Test Predictions [%d]" % i)
+
+        self.axes[0].imshow(raw)
+        cax1 = self.axes[1].imshow(label)
+        # self.fig.colorbar(cax1)
+        cax2 = self.axes[2].imshow(log_label)
+        # self.fig.colorbar(cax2)
+        cax3 = self.axes[3].imshow(coarse)
+        # self.fig.colorbar(cax3)
+        cax4 = self.axes[4].imshow(fine)
+        # self.fig.colorbar(cax4)
+
+        plt.pause(0.001)
+
+    @staticmethod
+    def plotTrainingProgress(raw, label, log_label, coarse, fine, figId):
+        fig = plt.figure(figId)
+        fig.clf()
+
+        # fig, axes = plt.subplots(5, 1)
+
+        ax = fig.add_subplot(3, 2, 1)
+        cax = ax.imshow(raw, cmap='gray')
+        plt.title("Raw")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 3)
+        cax = ax.imshow(label, cmap='gray')
+        plt.title("Label")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 5)
+        cax = ax.imshow(log_label, cmap='gray')
+        plt.title("log(Label)")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 2)
+        cax = ax.imshow(coarse, cmap='gray')
+        plt.title("Coarse")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 4)
+        cax = ax.imshow(fine, cmap='gray')
+        plt.title("Fine")
+        fig.colorbar(cax)
+
+        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
+        fig.canvas.set_window_title("Train Predictions")
+
+        plt.pause(0.001)
+
+    @staticmethod
+    # TODO: Add raw, log_labels, coarse
+    def displayValidationProgress(label, fine, figId):
+        fig = plt.figure(figId)
+        fig.clf()
+        ax = fig.add_subplot(2, 1, 1)
+        cax = ax.imshow(label, cmap='gray')
+        plt.title("valid_labels[0,:,:]")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(2, 1, 2)
+        cax = ax.imshow(fine, cmap='gray')
+        plt.title("vPredictions_f[0,:,:]")
+        fig.colorbar(cax)
+
+        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
+    @staticmethod
+    def plotTrainingErrorProgress(raw, label, coarse, fine, figId):
+        # Lembre que a Training Loss utilizaRMSE_log_scaleInv, porém o resultado é avaliado utilizando MSE
+        coarseMSE = loss.np_MSE(y=coarse, y_=label)
+        fineMSE = loss.np_MSE(y=fine, y_=label)
+
+        fig = plt.figure(figId)
+        fig.clf()
+
+        ax = fig.add_subplot(3, 2, 1)
+        cax = ax.imshow(label, cmap='gray')
+        plt.title("Label")
+        fig.colorbar(cax)
+
+        # TODO: Tentar resolver o problema que a imagem rgb não era correspondente com o label
+        # ax = fig.add_subplot(3, 2, 2)
+        # cax = ax.imshow(batch_data_crop_img)
+        # plt.title("batch_data_crop[0,:,:]")
+        # fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 3)
+        cax = ax.imshow(coarse, cmap='gray')
+        plt.title("Coarse")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 5)
+        cax = ax.imshow(fine, cmap='gray')
+        plt.title("Fine")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 4)
+        cax = ax.imshow(coarseMSE, cmap='jet')
+        plt.title("MSE(Coarse)")
+        fig.colorbar(cax)
+
+        ax = fig.add_subplot(3, 2, 6)
+        cax = ax.imshow(fineMSE, cmap='jet')
+        plt.title("MSE(Fine)")
+        fig.colorbar(cax)
+
+        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
+        fig = plt.gcf()  # TODO: Posso remover?
+        fig.canvas.set_window_title("Train Predictions + MSE")
+
+        plt.pause(0.001)
+
+
 def predict(model_data_path, image_path):
     # Default input size
     height = 228
@@ -830,6 +1033,13 @@ def train(args):
         # =================
         start = time.time()
 
+        if args.show_train_progress:
+            train_plotObj = Plot(args.mode, title='Train Predictions')
+
+        if args.show_valid_progress:
+            valid_plotObj = Plot(args.mode, title='Validation Prediction')
+
+
         batch_data = np.zeros((args.batch_size,
                                params['inputSize'][1],
                                params['inputSize'][2],
@@ -872,17 +1082,16 @@ def train(args):
             feed_dict_train = {tf_image: batch_data, tf_labels: batch_labels}
 
             # ----- Session Run! ----- #
-            _, log_labels, pred, train_loss = sess.run([train, tf_log_labels, net.get_output(), tf_loss], feed_dict=feed_dict_train)  # Training
+            _, log_labels, train_pred, train_loss = sess.run([train, tf_log_labels, net.get_output(), tf_loss], feed_dict=feed_dict_train)  # Training
             valid_loss = -1 # FIXME: value
             # -----
 
             # Prints Training Progress
             if step % 10 == 0:
                 if args.show_train_progress:
-                    # FIXME:
-                    # train_plotObj.showTrainResults(raw=batch_data_crop[0, :, :], label=batch_labels[0, :, :],
-                    #                                log_label=log_labels[0, :, :],
-                    #                                coarse=train_PredCoarse[0, :, :], fine=train_PredFine[0, :, :])
+                    train_plotObj.showTrainResults(raw=batch_data_crop[0, :, :], label=batch_labels[0, :, :],
+                                                   log_label=log_labels[0, :, :],
+                                                   pred=train_pred[0, :, :, 0])
 
                     # Plot.plotTrainingProgress(raw=batch_data_crop[0, :, :], label=batch_labels[0, :, :],log_label=log_labels[0, :, :], coarse=train_PredCoarse[0, :, :],fine=train_PredFine[0, :, :], fig_id=3)
                     pass
