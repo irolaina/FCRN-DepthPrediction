ada06ee02e8ab70ad2973deb506b345be57fcb41
Jefferson Souza <jrsouza@ufu.br>
2018-02-28 18:25:25 -0300

    [MOD] Camera Working

--- README_nick.md
+++ README_nick.md
@@ -16,9 +16,25 @@ Predict:
 
 Predict OpenCV:
 
-    python3 predict_cv.py ../models/NYU_FCRN-checkpoint/NYU_FCRN.ckpt ../misc/drone_indoor.avi
-    python3 predict_cv.py ../models/NYU_FCRN-checkpoint/NYU_FCRN.ckpt ../misc/drone_indoor.avi --gpu 1 2> error.txt
+    python3 predict_cv.py ../models/NYU_FCRN-checkpoint/NYU_FCRN.ckpt ../misc/drone_indoor.mp4
+    python3 predict_cv.py ../models/NYU_FCRN-checkpoint/NYU_FCRN.ckpt ../misc/drone_indoor.mp4 --gpu 1 2> error.txt
 
 Encode Video:
 
     ffmpeg -r 30 -f image2 -s 304x288 -i frame%06d.png -i pred%06d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ../test.mp4
+
+Dependencies:
+
+1) Gstreamer
+    
+    sudo apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
+
+2) ffmpeg
+
+    sudo apt install ffmpeg
+
+3) Grant access to user for using video devices
+
+    grep video /etc/group
+    sudo usermod -a -G video olorin
+    sudo chmod 777 /dev/video0
--- tensorflow/predict_cv.py
+++ tensorflow/predict_cv.py
@@ -44,8 +44,13 @@ def main():
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
     # Load from Camera or Video
-    # cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture(args.video_path)
+    cap = cv2.VideoCapture(0)
+    
+    if not cap.isOpened():  # Check if it succeeded
+        print("It wasn't possible to open the camera.")
+        return -1;
+
+    # cap = cv2.VideoCapture(args.video_path)
 
     # ----------------
     #  Building Graph
@@ -81,85 +86,81 @@ def main():
         while(True):
             # Capture frame-by-frame
             success, frame = cap.read()
-            if success: # Detect when the video finishes
-                frame = cv2.resize(frame,(304, 228), interpolation = cv2.INTER_CUBIC)
+            frame = cv2.resize(frame,(304, 228), interpolation = cv2.INTER_CUBIC)
 
-                # Read image
-                img = np.array(frame).astype('float32')
-                img = np.expand_dims(np.asarray(img), axis=0)
+            # Read image
+            img = np.array(frame).astype('float32')
+            img = np.expand_dims(np.asarray(img), axis=0)
 
-                # Evalute the network for the given image
-                pred_log, pred = sess.run([net.get_output(), tf_pred], feed_dict={input_node: img})
+            # Evalute the network for the given image
+            pred_log, pred = sess.run([net.get_output(), tf_pred], feed_dict={input_node: img})
 
-                # print(frame.shape, frame.dtype)
-                # print()
-                # print(pred_log)
-                # print(type(pred_log))
-                # print(pred_log.shape, pred_log.dtype)
-                # input("enter")
-                # print()
-                # print(pred)      
-                # print(type(pred))
-                # print(pred.shape, pred.dtype)
-                # input("enter2")
+            # print(frame.shape, frame.dtype)
+            # print()
+            # print(pred_log)
+            # print(type(pred_log))
+            # print(pred_log.shape, pred_log.dtype)
+            # input("enter")
+            # print()
+            # print(pred)      
+            # print(type(pred))
+            # print(pred.shape, pred.dtype)
+            # input("enter2")
 
-                # Barely Observed, Pred range (0, 12000)
-                def check_min_max_values():
-                    min = np.min(pred)
-                    max = np.max(pred)
+            # Barely Observed, Pred range (0, 12000)
+            def check_min_max_values():
+                min = np.min(pred)
+                max = np.max(pred)
 
-                    if isFirstTime:
-                        min_min = min 
-                        max_max = max
-                        isFirstTime = False
+                if isFirstTime:
+                    min_min = min 
+                    max_max = max
+                    isFirstTime = False
 
-                    if min < min_min:
-                        min_min = min
-                    
-                    if max > max_max:
-                        max_max = max
-
-                    print()
-                    print("min:", min)      
-                    print("max:", max)
-                    
-                # check_min_max_values()
+                if min < min_min:
+                    min_min = min
                 
-                pred_resized = cv2.resize(pred[0],(304, 228), interpolation = cv2.INTER_CUBIC)
-                pred_uint8 = cv2.convertScaleAbs(pred_resized)
+                if max > max_max:
+                    max_max = max
 
-                # print(pred_resized.shape)
-                # print(pred_uint8.shape)
+                print()
+                print("min:", min)      
+                print("max:", max)
+                
+            # check_min_max_values()
+            
+            pred_resized = cv2.resize(pred[0],(304, 228), interpolation = cv2.INTER_CUBIC)
+            pred_uint8 = cv2.convertScaleAbs(pred_resized)
 
-                # print()
-                # print(pred_uint8[0,:,:,0])
-                # print(np.min(pred_uint8))
-                # print(np.max(pred_uint8))
-                # print(pred_uint8.shape, pred_uint8.dtype)
-                # input("pred_uint8")
+            # print(pred_resized.shape)
+            # print(pred_uint8.shape)
 
-                # Display the resulting frame - Matplotlib
-                # plt.figure(1)
-                # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) # OpenCV uses BGR, Matplotlib uses RGB
-                # plt.figure(2)
-                # plt.imshow(pred[0, :, :, 0])
-                # plt.pause(0.001)
+            # print()
+            # print(pred_uint8[0,:,:,0])
+            # print(np.min(pred_uint8))
+            # print(np.max(pred_uint8))
+            # print(pred_uint8.shape, pred_uint8.dtype)
+            # input("pred_uint8")
 
-                # Display the resulting frame - OpenCV
-                pred_uint8_inv = 255-pred_uint8
-                cv2.imshow('frame',frame)
-                cv2.imshow('pred', pred_uint8_inv) # Colors inverted only for visual Pro
+            # Display the resulting frame - Matplotlib
+            # plt.figure(1)
+            # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) # OpenCV uses BGR, Matplotlib uses RGB
+            # plt.figure(2)
+            # plt.imshow(pred[0, :, :, 0])
+            # plt.pause(0.001)
 
-                # Save Images
-                if SAVE_IMAGES:
-                    cv2.imwrite("output/fcrn_cv/frame%06d.png" % count, frame);
-                    cv2.imwrite("output/fcrn_cv/pred%06d.png" % count, pred_uint8_inv)
-                    count += 1
+            # Display the resulting frame - OpenCV
+            pred_uint8_inv = 255-pred_uint8
+            cv2.imshow('frame',frame)
+            cv2.imshow('pred', pred_uint8_inv) # Colors inverted only for visual Pro
 
-                if cv2.waitKey(1) & 0xFF == ord('q'): # without waitKey() the images are not shown.
-                    break
+            # Save Images
+            if SAVE_IMAGES:
+                cv2.imwrite("output/fcrn_cv/frame%06d.png" % count, frame);
+                cv2.imwrite("output/fcrn_cv/pred%06d.png" % count, pred_uint8_inv)
+                count += 1
 
-            else:
+            if cv2.waitKey(1) & 0xFF == ord('q'): # without waitKey() the images are not shown.
                 break
 
     # When everything done, release the capture
