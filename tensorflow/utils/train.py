4242b472c939be8176ed3447adb82b1c1cfa6be1
nicolasrosa <nicolas.rosa@hotmail.com>
2018-04-11 17:17:37 -0300

    [MOD] Early Stopping class moved!

--- tensorflow/utils/train.py
+++ tensorflow/utils/train.py
@@ -1,13 +1,20 @@
 # ===========
 #  Libraries
 # ===========
+import numpy as np
 import tensorflow as tf
+
+from collections import deque
 
 # ==================
 #  Global Variables
 # ==================
 LOSS_LOG_INITIAL_VALUE = 0.1
 
+# Early Stop Configuration
+AVG_SIZE = 20
+MIN_EVALUATIONS = 1000
+MAX_STEPS_AFTER_STABILIZATION = 10000
 
 # ===================
 #  Class Declaration
@@ -48,3 +55,33 @@ class Train:
         tf.add_to_collection('labels', self.tf_labels)
         tf.add_to_collection('global_step', self.tf_global_step)
         tf.add_to_collection('learning_rate', self.tf_learningRate)
+
+# TODO: Move
+# TODO: Validar
+class EarlyStopping:
+    def __init__(self):
+        # Local Variables
+        self.movMeanLast = 0
+        self.movMean = deque()
+        self.stabCounter = 0
+
+    def check(self, step, valid_loss):
+        self.movMean.append(valid_loss)
+
+        if step > AVG_SIZE:
+            self.movMean.popleft()
+
+        movMeanAvg = np.sum(self.movMean) / AVG_SIZE
+        movMeanAvgLast = np.sum(self.movMeanLast) / AVG_SIZE
+
+        if (movMeanAvg >= movMeanAvgLast) and step > MIN_EVALUATIONS:
+            # print(step,stabCounter)
+
+            self.stabCounter += 1
+            if self.stabCounter > MAX_STEPS_AFTER_STABILIZATION:
+                print("\nSTOP TRAINING! New samples may cause overfitting!!!")
+                return 1
+        else:
+            self.stabCounter = 0
+
+            self.movMeanLast = deque(self.movMean)
