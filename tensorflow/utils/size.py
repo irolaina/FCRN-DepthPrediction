a706de134f6925709712b1cb7507e82d00423a60
nicolasrosa <nicolas.rosa@hotmail.com>
2018-04-11 10:53:09 -0300

    [MOD] Size class created!

---
+++ tensorflow/utils/size.py
@@ -0,0 +1,10 @@
+# ===================
+#  Class Declaration
+# ===================
+class Size:
+    def __init__(self, height, width, nchannels):
+        self.height = height
+        self.width = width
+        self.nchannels = nchannels
+
+        print("Size object created.")
