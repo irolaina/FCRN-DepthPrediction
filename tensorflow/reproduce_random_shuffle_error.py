e0a9ea229552be7e26caf707e60ab3de1c00996c
nicolasrosa <nicolas.rosa@hotmail.com>
2018-05-22 14:12:10 -0300

    [MOD] reproduce_random_shuffler_error updated.

--- tensorflow/reproduce_random_shuffle_error.py
+++ tensorflow/reproduce_random_shuffle_error.py
@@ -1,18 +1,29 @@
-# ========
-#  README
-# ========
-# FIXME: RandomShuffleError
+# =============================
+#  README - RandomShuffleError
+# =============================
 # Informações
 # - Acontece na epoch: 1244, batch_size = 4
 # - Acontece na epoch: 622, batch_size = 8
 # - Não acontece com batch de strings
 # - Não acontece com as images uint8, apenas com as depth uint16
 
+# Dúvidas
+# TODO: Por que string_input_producer sempre começa do segundo sample?
+# - Será que alguma imagem da lista de imagens não existe? R.: Usei o check_filenames_exists.py e todas as imagens existem
+# - Será que o erro está sendo causado por tf_image ser uint8 e tf_depth ser uint16? R.: Mudei pra float32 e continuou dando erro
+# - Um dos erros que deu indicava que talvez uma das imagens de depth tivesse formato inválido, isto é, que não fosse
+#   PNG. R.: Código conseguiu ler todas as imagens.
+
+# Solução
+# - Todas as imagens das listas existiam, porém uma delas estava corrompida.
+# - No linux, o arquivo era identificado como um arquivo do tipo 'text'.
+# - Imagem corrompida: ../Depth/Record011/Camera 5/171206_055536605_Camera_5.png
+
 # Testes
 # TODO: [Ok] All Datasets/StringsBatch/train.batch
 # TODO: [Ok] All Datasets/StringsBatch/train.random_shuffle_batch
 
-# TODO: [  ] KittiContinuous/ImagesBatch/train.batch/Ambos/Juntos
+# TODO: [Ok] KittiContinuous/ImagesBatch/train.batch/Ambos/Juntos
 # TODO: [Ok] KittiContinuous/ImagesBatch/train.random_shuffle_batch/Ambos/Juntos !!!
 
 # TODO: [Ok] NyuDepth/ImagesBatch/train.batch/Ambos/Juntos
@@ -27,12 +38,6 @@
 # TODO: [  ] Apolloscape/ImagesBatch/train.random_shuffle_batch/Ambos/Separados -> DESSINCRONIZA!
 # TODO: [  ] Apolloscape/ImagesBatch/train.random_shuffle_batch/OnlyImage
 # TODO: [  ] Apolloscape/ImagesBatch/train.random_shuffle_batch/OnlyDepth
-
-# Dúvidas
-# TODO: Por que string_input_producer sempre começa do segundo sample?
-# Será que alguma imagem da lista de imagens não existe? R.: Usei o check_filenames_exists.py e todas as imagens existem
-# Será que o erro está sendo causado por tf_image ser uint8 e tf_depth ser uint16? R.: Mudei pra float32 e continuou dando erro
-# Um dos erros que deu indicava que talvez uma das imagens de depth tivesse formato inválido, isto é, que não fosse PNG. R.: Código conseguiu ler todas as imagens.
 
 # ===========
 #  Libraries
@@ -56,14 +61,15 @@ capacity = min_after_dequeue + num_threads * batch_size
 #  Read Input File
 # =================
 # Select Dataset:
-# dataset_name = 'apolloscape'
-dataset_name = 'kitticontinuous_residential'
+dataset_name = 'apolloscape'
+# dataset_name = 'kitticontinuous_residential'
 # dataset_name = 'nyudepth'
 
 if dataset_name == 'apolloscape':
     data = np.genfromtxt('data/apolloscape_train.txt', dtype='str', delimiter='\t')
     # data = np.genfromtxt('tmp/apolloscape_train_little.txt', dtype='str', delimiter='\t')
     # data = np.genfromtxt('tmp/apolloscape_train_little2.txt', dtype='str', delimiter='\t')
+    # data = np.genfromtxt('tmp/apolloscape_train_little3.txt', dtype='str', delimiter='\t')
 
     image_shape = (2710, 3384, 3)
     depth_shape = (2710, 3384, 1)
@@ -119,33 +125,33 @@ else:
     raise SystemExit
 
 # Retrieves Shape
-tf_image.set_shape(image_shape)
-tf_depth.set_shape(depth_shape)
+# tf_image.set_shape(image_shape)
+# tf_depth.set_shape(depth_shape)
 
 # Change Type
 # tf_image = tf.cast(tf_image, tf.float32) # uint8  -> float32
 # tf_depth = tf.cast(tf_depth, tf.float32) # uint16 -> float32
 
-# tf_image_shape = tf.shape(tf_image) # Test 6
-# tf_depth_shape = tf.shape(tf_depth)
+tf_image_shape = tf.shape(tf_image)
+tf_depth_shape = tf.shape(tf_depth)
 
 # =========================
 #  Prepare Batch - Select:
 # =========================
 # Strings Batch
-# tf_batch_image, tf_batch_depth = tf.train.batch([tf_image_key, tf_depth_key], batch_size, num_threads, capacity)  # Test 2
-# tf_batch_image, tf_batch_depth = tf.train.shuffle_batch([tf_image_key, tf_depth_key], batch_size, capacity, min_after_dequeue, num_threads)  # Test 3
+# tf_batch_image, tf_batch_depth = tf.train.batch([tf_image_key, tf_depth_key], batch_size, num_threads, capacity)
+# tf_batch_image, tf_batch_depth = tf.train.shuffle_batch([tf_image_key, tf_depth_key], batch_size, capacity, min_after_dequeue, num_threads)
 
 # Images Batch
 # Junto
-tf_batch_image, tf_batch_depth = tf.train.batch([tf_image, tf_depth], batch_size, num_threads, capacity, shapes=[image_shape, depth_shape]) # Test 4
-# tf_batch_image, tf_batch_depth = tf.train.shuffle_batch([tf_image, tf_depth], batch_size, capacity, min_after_dequeue, num_threads) # Test 5
+tf_batch_image, tf_batch_depth = tf.train.batch([tf_image, tf_depth], batch_size, num_threads, capacity, shapes=[image_shape, depth_shape])
+# tf_batch_image, tf_batch_depth = tf.train.shuffle_batch([tf_image, tf_depth], batch_size, capacity, min_after_dequeue, num_threads)
 
 # Separado
-# tf_batch_image = tf.train.batch([tf_image], batch_size, num_threads, capacity)  # Test 4
-# tf_batch_depth = tf.train.batch([tf_depth], batch_size, num_threads, capacity)  # Test 4
-# tf_batch_image = tf.train.shuffle_batch([tf_image], batch_size, capacity, min_after_dequeue, num_threads) # Test 5
-# tf_batch_depth = tf.train.shuffle_batch([tf_depth], batch_size, capacity, min_after_dequeue, num_threads) # Test 5
+# tf_batch_image = tf.train.batch([tf_image], batch_size, num_threads, capacity)
+# tf_batch_depth = tf.train.batch([tf_depth], batch_size, num_threads, capacity)
+# tf_batch_image = tf.train.shuffle_batch([tf_image], batch_size, capacity, min_after_dequeue, num_threads)
+# tf_batch_depth = tf.train.shuffle_batch([tf_depth], batch_size, capacity, min_after_dequeue, num_threads)
 
 # Print Tensors
 print("\nTensors:")
@@ -155,15 +161,17 @@ print("tf_image_file: \t", tf_image_file)
 print("tf_depth_file: \t", tf_depth_file)
 print("tf_image: \t", tf_image)
 print("tf_depth: \t", tf_depth)
-print("tf_batch_image: ", tf_batch_image)
-print("tf_batch_depth: ", tf_batch_depth)
+print("tf_image_shape: \t", tf_image_shape)
+print("tf_depth_shape: \t", tf_depth_shape)
+# print("tf_batch_image: ", tf_batch_image)
+# print("tf_batch_depth: ", tf_batch_depth)
 input("enter")
 
 # =====
 #  Run
 # =====
 numEpochs = 2000
-# numEpochs = 102687 # numSamples
+# numEpochs = 102686 # numSamples
 with tf.Session() as sess:
     # ----- Tensors Initialization ----- #
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
@@ -179,7 +187,6 @@ with tf.Session() as sess:
         #  Without Batch
         # ===============
         # image_key, image, depth_key, depth = sess.run([tf_image_key, tf_image, tf_depth_key, tf_depth])
-        # image_shape, depth_shape = sess.run([tf_image_shape, tf_depth_shape])
 
         # print("image:")
         # print(image_key, len(image_key))
@@ -188,8 +195,9 @@ with tf.Session() as sess:
         # print(depth_key, len(depth_key))
         # print(depth.shape)
 
-        # print(image_shape)
-        # print(depth_shape)
+        # image_key, image_shape, depth_key, depth_shape = sess.run([tf_image_key, tf_image_shape, tf_depth_key, tf_depth_shape])
+        # print(image_key, image_shape)
+        # print(depth_key, depth_shape)
 
         # ============
         #  With Batch
