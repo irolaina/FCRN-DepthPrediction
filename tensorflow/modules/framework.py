2732bd2dc9bff5d65a3375e2f23950a1cbbba212
nicolasrosa <nicolas.rosa@hotmail.com>
2018-06-19 12:39:00 -0300

    [MOD] MajorFix rawdepth2meters() implemented on validation! 2

--- tensorflow/modules/framework.py
+++ tensorflow/modules/framework.py
@@ -102,7 +102,7 @@ class Model(object):
             if valid_pixels:
                 print("[Network/Loss] Compute: Ignore invalid pixels")
             else:
-                print("\n[Network/Loss] Loss: All Pixels")
+                print("[Network/Loss] Loss: All Pixels")
             print("[Network/Loss] Loss Function: %s" % self.loss_name)
 
     def build_optimizer(self):
@@ -128,8 +128,8 @@ class Model(object):
         with tf.name_scope("Valid"):
             tf.summary.scalar('loss', self.valid.tf_loss, collections=self.model_collection)
 
-            tf.summary.image('input/image', self.valid.tf_image, max_outputs=1, collections=self.model_collection)
-            tf.summary.image('input/depth', self.valid.tf_depth, max_outputs=1, collections=self.model_collection)
+            tf.summary.image('input/image', self.valid.tf_image2, max_outputs=1, collections=self.model_collection)
+            tf.summary.image('input/depth', self.valid.tf_depth2, max_outputs=1, collections=self.model_collection)
             tf.summary.image('input/image_resized', self.valid.tf_image_resized, max_outputs=1, collections=self.model_collection)
             tf.summary.image('input/depth_resized', self.valid.tf_depth_resized, max_outputs=1, collections=self.model_collection)
 
--- tensorflow/modules/validation.py
+++ tensorflow/modules/validation.py
@@ -18,37 +18,41 @@ from .dataloader import Dataloader
 class Validation:
     def __init__(self, args, input_size, output_size, max_depth, dataset_name):
         # Raw Input/Output
-        self.tf_image = tf.placeholder(tf.uint8, shape=(None, None, None, 3))
+        self.tf_image_raw = tf.placeholder(tf.uint8, shape=(None, None, None, 3))
 
         if dataset_name.split('_')[0] == 'kittidiscrete' or \
            dataset_name.split('_')[0] == 'kitticontinuous':
-            self.tf_depth = tf.placeholder(tf.uint8, shape=(None, None, None, 1))
+            self.tf_depth_raw = tf.placeholder(tf.uint8, shape=(None, None, None, 1))
         else:
-            self.tf_depth = tf.placeholder(tf.uint16, shape=(None, None, None, 1))
+            self.tf_depth_raw = tf.placeholder(tf.uint16, shape=(None, None, None, 1))
 
         # True Depth Value Calculation. May vary from dataset to dataset.
-        self.tf_depth = Dataloader.rawdepth2meters(args.dataset, self.tf_depth)
+        self.tf_depth3 = Dataloader.rawdepth2meters(args.dataset, self.tf_depth_raw)
 
         # Convert uint8/uint16 to float32
-        self.tf_image = tf.cast(self.tf_image, tf.float32, name='image')
-        self.tf_depth = tf.cast(self.tf_depth, tf.float32, name='depth')
+        self.tf_image_raw = tf.cast(self.tf_image_raw, tf.float32, name='image')
+        # self.tf_depth = tf.cast(self.tf_depth, tf.float32, name='depth')
 
         # Workaround for assigning bug
-        self.tf_image2 = self.tf_image
-        self.tf_depth2 = self.tf_depth
+        self.tf_image2 = self.tf_image_raw
+        self.tf_depth2 = self.tf_depth3
 
         # Crops Input and Depth Images (Removes Sky)
         if dataset_name[0:5] == 'kitti':
-            tf_image_shape = tf.shape(self.tf_image)
-            tf_depth_shape = tf.shape(self.tf_depth)
+            tf_image_shape = tf.shape(self.tf_image_raw)
+            tf_depth_shape = tf.shape(self.tf_depth_raw)
 
             crop_height_perc = tf.constant(0.3, tf.float32)
             tf_image_new_height = crop_height_perc * tf.cast(tf_image_shape[1], tf.float32)
             tf_depth_new_height = crop_height_perc * tf.cast(tf_depth_shape[1], tf.float32)
 
             # FIXME: Why changing to self.tf_image e self.tf_depth doesn't work?
-            self.tf_image2 = self.tf_image[:, tf.cast(tf_image_new_height, tf.int32):, :]
-            self.tf_depth2 = self.tf_depth[:, tf.cast(tf_depth_new_height, tf.int32):, :]
+            self.tf_image2 = self.tf_image_raw[:, tf.cast(tf_image_new_height, tf.int32):, :]
+            self.tf_depth2 = self.tf_depth_raw[:, tf.cast(tf_depth_new_height, tf.int32):, :]
+
+        print(self.tf_image2)
+        print(self.tf_depth2)
+        input("valid")
 
         # Downsizes Input and Depth Images
         self.tf_image_resized = tf.image.resize_images(self.tf_image2, [input_size.height, input_size.width])
@@ -72,8 +76,8 @@ class Validation:
             self.plot = Plot(args.mode, title='Validation Prediction')
 
         print("[Network/Validation] Validation Tensors created.")
-        print(self.tf_image)
-        print(self.tf_depth)
+        print(self.tf_image2)
+        print(self.tf_depth2)
         print(self.tf_image_resized)
         print(self.tf_image_resized_uint8)
         print(self.tf_depth_resized)
