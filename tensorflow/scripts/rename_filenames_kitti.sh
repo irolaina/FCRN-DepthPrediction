533e8c4b166cdef77f86cab800c789993a84a233
nicolasrosa <nicolas.rosa@hotmail.com>
2018-06-26 10:38:14 -0300

    [ADD] rename_filenames_kitti.sh

---
+++ tensorflow/scripts/rename_filenames_kitti.sh
@@ -0,0 +1,3 @@
+find -name 'city_*' -exec bash -c 'mv $0 ${0/city_}' {} \;
+find -name 'residential_*' -exec bash -c 'mv $0 ${0/residential_}' {} \;
+find -name 'campus_*' -exec bash -c 'mv $0 ${0/campus_}' {} \;
