ab0d60a1b38e3ac3e264a3e4c9f5ff026bb52c81
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-05 18:25:16 -0300

    [MOD] Report Bug

--- tensorflow/predict_nick.py
+++ tensorflow/predict_nick.py
@@ -24,6 +24,7 @@
 # Known Bugs
 # [Train] FIXME: Resolver erro que acontece com as imagens do ApolloScape durante valid evaluation @ ~24000
 # [Train] FIXME: KittiDiscrete tem problemas de convergência quando utiliza-se a flag --px all
+# [Train] FIXME: python3 predict_nick.py -m train --machine xps -s kittidiscrete --px all --loss silog --max_steps 150000 --ldecay --l2norm --remove_sky -t
 # [All] TODO: Devo continuar usando tf.image.resize_images(), há relatos desta função ser bugada
 
 # Optional
