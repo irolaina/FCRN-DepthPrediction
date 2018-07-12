31487fd91f8c13e63b18f024c65a2561efde7e57
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-12 16:48:24 -0300

    [MOD] detect_available_models() sort() added

--- tensorflow/modules/utils.py
+++ tensorflow/modules/utils.py
@@ -63,6 +63,7 @@ def detect_available_models(args):
 
     if args.model_path == '':
         found_models = glob.glob("output/fcrn/*/*/*/*/restore/*.meta")
+        found_models.sort()
 
         for i, model in enumerate(found_models):
             print(i, model)
