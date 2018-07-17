dc9e8e43f5f5d58a3d8a3452a75f674222d0b44b
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-17 16:12:48 -0300

    [MOD] run_testX.sh updated

--- tensorflow/run_test1.sh
+++ tensorflow/run_test1.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 GPU=0
-python3 predict_nick.py -m train --machine $USER --gpu $GPU -s apolloscape --max_steps 150000 --ldecay --l2norm -t
-python3 predict_nick.py -m train --machine $USER --gpu $GPU -s kittidepth --loss mse --max_steps 150000 --ldecay --l2norm
-python3 predict_nick.py -m train --machine $USER --gpu $GPU -s kittidiscrete --loss mse --max_steps 150000 --ldecay --l2norm
-python3 predict_nick.py -m train --machine $USER --gpu $GPU -s kitticontinuous --loss mse --max_steps 150000 --ldecay --l2norm
-python3 predict_nick.py -m train --machine $USER --gpu $GPU -s nyudepth --max_steps 150000 --ldecay --l2norm -t
+STEPS = 150000
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s apolloscape --max_steps ${STEPS} --ldecay --l2norm -t
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --loss mse --max_steps ${STEPS} --ldecay --l2norm
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --loss mse --max_steps ${STEPS} --ldecay --l2norm
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --loss mse --max_steps ${STEPS} --ldecay --l2norm
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --max_steps ${STEPS} --ldecay --l2norm -t
--- tensorflow/run_test2.sh
+++ tensorflow/run_test2.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-python3 predict_nick.py -m train --machine $USER --gpu 1 -s nyudepth --loss mse --max_steps 150000 --ldecay --l2norm
-python3 predict_nick.py -m train --machine $USER --gpu 1 -s nyudepth --loss berhu --max_steps 150000 --ldecay --l2norm
-python3 predict_nick.py -m train --machine $USER --gpu 1 -s nyudepth --loss mse --max_steps 150000 --ldecay --l2norm
-python3 predict_nick.py -m train --machine $USER --gpu 1 -s nyudepth --loss berhu --max_steps 150000 --ldecay --l2norm
-
+GPU=1
+STEPS = 150000
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss mse --max_steps ${STEPS} --ldecay --l2norm
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss berhu --max_steps ${STEPS} --ldecay --l2norm
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss mse --max_steps ${STEPS} --ldecay --l2norm
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss berhu --max_steps ${STEPS} --ldecay --l2norm
