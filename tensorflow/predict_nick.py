9e2ec4783451091f39d052b9d1c341b69a16eb73
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-19 13:52:05 -0300

    [MOD] predict_nick.py and run_test.sh updated

--- tensorflow/predict_nick.py
+++ tensorflow/predict_nick.py
@@ -23,8 +23,6 @@
 
 # Known Bugs
 # [Train] FIXME: Resolver erro que acontece com as imagens do ApolloScape durante valid evaluation @ ~24000
-# [Train] FIXME: python3 predict_nick.py -m train --machine nicolas -s kittidepth --px all --loss berhu --max_steps 150000 --ldecay --l2norm --remove_sky  -t
-# [Train] FIXME: python3 predict_nick.py -m train --machine nicolas -s kittidiscrete --px all --loss berhu --max_steps 150000 --ldecay --l2norm --remove_sky -t
 # [All] TODO: Devo continuar usando tf.image.resize_images(), há relatos desta função ser bugada
 
 # Optional
--- tensorflow/run_test.sh
+++ tensorflow/run_test.sh
@@ -13,14 +13,14 @@ STEPS=300000
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --px valid --loss berhu --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --px valid --loss silog --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Avoid! Does the Gradients calculation make sense for the 'valid' flag?
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --px all   --loss mse   --max_steps ${STEPS} --ldecay --l2norm --data_aug
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --px all   --loss berhu --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Doesn't converge!
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --px all   --loss berhu --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth --px all   --loss silog --max_steps ${STEPS} --ldecay --l2norm --data_aug
 
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px valid --loss mse   --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px valid --loss berhu --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px valid --loss silog --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Avoid! Does the Gradients calculation make sense for the 'valid' flag?
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px all   --loss mse   --max_steps ${STEPS} --ldecay --l2norm --data_aug
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px all   --loss berhu --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Doesn't converge!
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px all   --loss berhu --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete --px all   --loss silog --max_steps ${STEPS} --ldecay --l2norm --data_aug
 
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss mse   --max_steps ${STEPS} --ldecay --l2norm --data_aug
