d92f8c19bb115828a864c809c64a6c6c8aff0b69
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-19 11:22:06 -0300

    [MOD] run_testX.sh updated

--- tensorflow/run_test2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-GPU=1
-STEPS = 150000
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss mse --max_steps ${STEPS} --ldecay --l2norm
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss berhu --max_steps ${STEPS} --ldecay --l2norm
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss mse --max_steps ${STEPS} --ldecay --l2norm
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth --loss berhu --max_steps ${STEPS} --ldecay --l2norm
