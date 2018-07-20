f1dc7587339133b971abea1defdf6a1d6e16a166
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-20 16:41:47 -0300

    [MOD] get_file_path() created

--- tensorflow/modules/datasets/dataset.py
+++ tensorflow/modules/datasets/dataset.py
@@ -22,3 +22,17 @@ class Dataset(FilenamesHandler):
         self.depth_size = Size(height, width, 1)
 
         print("[Dataloader] %s object created." % self.name)
+
+    def get_file_path(self, mode, test_split, test_file_path): # TODO: change name
+        if test_split == 'eigen':
+            file = 'modules/third_party/monodepth/utils/filenames/eigen_test_files.txt'
+        elif test_split == 'kitti':
+            file = 'modules/third_party/monodepth/utils/filenames/kitti_stereo_2015_test_files.txt'
+            self.dataset_path = '/media/nicolas/nicolas_seagate/datasets/kitti/stereo/stereo2015/data_scene_flow/'
+        else:
+            if test_file_path == '':
+                file = 'data/' + self.name + '_' + mode + '.txt'
+            else:
+                file = test_file_path
+
+        return file
