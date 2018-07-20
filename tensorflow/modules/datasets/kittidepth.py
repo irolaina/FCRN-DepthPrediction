3ea1fe1d041dfb3fa269c1055b9c706cf663517a
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-20 18:57:46 -0300

    [FIX] kittidepth.py, test_file.txt not found.

--- tensorflow/modules/datasets/kittidepth.py
+++ tensorflow/modules/datasets/kittidepth.py
@@ -46,11 +46,11 @@ class KittiDepth(Dataset):
 
 
     def getFilenamesLists(self, mode, test_split='', test_file_path=''):
-        file = self.get_file_path(mode, test_split, test_file_path)
-
         # Workaround # FIXME: Temporary
         if mode == 'test':
             mode = 'val'
+
+        file = self.get_file_path(mode, test_split, test_file_path)
 
         if os.path.exists(file):
             image_filenames, depth_filenames = self.read_text_file(file, self.dataset_path)
