21416e4e550c61ee76f8b439b00da8627457e592
nicolasrosa <nicolas.rosa@hotmail.com>
2018-07-30 11:15:01 -0300

    [MOD] results_metrics.txt and run_test.sh updated

--- tensorflow/run_test.sh
+++ tensorflow/run_test.sh
@@ -16,9 +16,9 @@ STEPS=300000
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px valid --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px valid --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Avoid! Does the Gradients calculation make sense for the 'valid' flag?
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px all   --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px all   --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
+#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px all   --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px all   --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px all   --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug
+#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth      --px all   --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug
 
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px valid --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px valid --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
@@ -27,15 +27,15 @@ python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidepth   
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px all   --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px all   --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px all   --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
-python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px all   --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug
+#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kittidiscrete   --px all   --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug
 
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
-#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
-#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
-#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Avoid! Does the Gradients calculation make sense for the 'valid' flag?
-#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
-#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
-#python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px valid --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug  # Avoid! Does the Gradients calculation make sense for the 'valid' flag?
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss berhu       --max_steps ${STEPS} --ldecay --l2norm --data_aug
+python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss eigen       --max_steps ${STEPS} --ldecay --l2norm --data_aug
 python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s kitticontinuous --px all   --loss eigen_grads --max_steps ${STEPS} --ldecay --l2norm --data_aug
 
 #python3 predict_nick.py -m train --machine ${USER} --gpu ${GPU} -s nyudepth        --px valid --loss mse         --max_steps ${STEPS} --ldecay --l2norm --data_aug
