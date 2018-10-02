9da22e33d09d4813982877bd01a9d763e49b297a
nicolasrosa <nicolas.rosa@hotmail.com>
2018-10-02 15:08:50 -0300

    [MOD] ros_catkin_ws/build/ folders updated

---
+++ tensorflow/ros_catkin_ws/build/fcrn/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nicolas/go_fcrn/ros_catkin_ws/src/fcrn
+# Build directory: /home/nicolas/MEGA/workspace/FCRN-DepthPrediction/tensorflow/ros_catkin_ws/build/fcrn
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
