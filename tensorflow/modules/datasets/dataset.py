8e4eb4a98176a0c926b921990d66199ec9579331
nicolasrosa <nicolas.rosa@hotmail.com>
2019-03-11 14:15:49 -0300

    [MOD] dataset.py updated

--- tensorflow/modules/datasets/dataset.py
+++ tensorflow/modules/datasets/dataset.py
@@ -27,21 +27,49 @@ class Dataset(FilenamesHandler):
 
         print("[Dataloader] %s object created." % self.name)
 
+    # FIXME: Isto esta função está correta?
+    # Acredito que ainda seja necessário arrumar a combinação dos eval_tool e os test_splits. Ou indepente qual eval tool está sendo usado quando não se especifica o test_split?
     def get_file_path(self, mode, test_split, test_file_path):
-        if test_split == 'eigen' or test_split == 'eigen_continuous':
+        # KITTI Stereo 2015: 200 Test Images
+        # Eigen Split: 697 Test Images
+        # Eigen Split & KITTI Depth: 652 Test Images
+
+        # ------------------------------------------------------------- #
+        #  Evaluation based on Disparity Images (Eval Tool: MonoDepth)  #
+        # ------------------------------------------------------------- #
+        if args.eval_tool == 'monodepth' and (test_split == 'eigen' or test_split == 'eigen_continuous'):
             file_path = 'modules/third_party/monodepth/utils/filenames/eigen_test_files.txt'
 
             # Overwrite the 'dataset_path' specified by the dataset
             self.dataset_path = '/media/nicolas/nicolas_seagate/datasets/kitti/raw_data/'
 
-        elif test_split == 'kitti_stereo':
+        elif args.eval_tool == 'monodepth' and test_split == 'kitti_stereo':
             file_path = 'modules/third_party/monodepth/utils/filenames/kitti_stereo_2015_test_files.txt'
 
             # Overwrite the 'dataset_path' specified by the dataset
             self.dataset_path = '/media/nicolas/nicolas_seagate/datasets/kitti/stereo/stereo2015/data_scene_flow/'
 
-        elif test_split == 'eigen_kitti_depth':  # FIXME: Validar! Terminar!
-            file_path = 'data/new_splits/kitti_splits_based_on_monodepth_files/kitti_depth/eigen_test_files.txt'
+        elif args.eval_tool == 'monodepth' and test_split == 'eigen_kitti_depth':
+            file_path = 'data/new_splits/eigen_split_based_on_kitti_depth/eigen_test_kitti_depth_files.txt'
+
+            # Overwrite the 'dataset_path' specified by the dataset
+            self.dataset_path = '/media/nicolas/nicolas_seagate/datasets/kitti/'
+
+        # --------------------------------------------------------------------------------- #
+        #  Evaluation based on Ground Truth/Velodyne Scans Images (Eval Tool: KITTI Depth)  #
+        # --------------------------------------------------------------------------------- #
+        # FIXME:
+        elif args.eval_tool == 'kitti_depth' and (test_split == 'eigen' or test_split == 'eigen_continuous'):
+            print("Não deveria rodar! Terminar Implementação. Devo gerar os mapas de profundidade para que possa ser avaliado.")
+            raise SystemError
+
+        # FIXME:
+        elif args.eval_tool == 'kitti_depth' and test_split == 'kitti_stereo':
+            print("Não deveria rodar! Terminar Implementação. Devo gerar os mapas de profundidade para que possa ser avaliado.")
+            raise SystemError
+
+        elif args.eval_tool == 'kitti_depth' and test_split == 'eigen_kitti_depth':
+            file_path = 'data/new_splits/eigen_split_based_on_kitti_depth/eigen_test_kitti_depth_files.txt'
 
             # Overwrite the 'dataset_path' specified by the dataset
             self.dataset_path = '/media/nicolas/nicolas_seagate/datasets/kitti/'
