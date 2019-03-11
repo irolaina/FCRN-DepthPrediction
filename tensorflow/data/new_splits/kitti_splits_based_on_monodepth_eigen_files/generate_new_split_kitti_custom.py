2e2056b4afefc41885240d3f1d73dc01196148be
nicolasrosa <nicolas.rosa@hotmail.com>
2019-03-11 15:50:56 -0300

    [MOD] Removing Duplicated file.

--- tensorflow/data/new_splits/kitti_splits_based_on_monodepth_eigen_files/generate_new_split_kitti_custom.py
+++ tensorflow/data/new_splits/kitti_splits_based_on_monodepth_eigen_files/generate_new_split_kitti_custom.py
@@ -1,10 +1,20 @@
 import numpy as np
 
+# select_dataset = 'kitti_discrete'
+select_dataset = 'kitti_continuous'
+
+if select_dataset == 'kitti_discrete':
+    disp_folder = 'disp1'
+elif select_dataset == 'kitti_continuous':
+    disp_folder = 'disp2'
+else:
+    raise SystemError
+
 # ======= #
 #  Train  #
 # ======= #
 # Read Monodepths Files
-eigen_train_file = open("../monodepth/eigen_train_files.txt", "r")
+eigen_train_file = open("../../../modules/third_party/monodepth/utils/filenames/eigen_train_files.txt", "r")
 eigen_train = []
 
 for line in eigen_train_file:
@@ -29,21 +39,21 @@ for line in eigen_train_file:
     # print(splitted[1])
     # print(splitted)
 
-    eigen_train.append([newline, newline.replace('imgs', 'disp1')])
+    eigen_train.append([newline, newline.replace('imgs', disp_folder)])
 
 # Display
 for i, pair in enumerate(eigen_train):
     print(i, pair)
 
 # Save
-np.savetxt('kitti_discrete/eigen_train_files.txt', np.array(eigen_train), fmt='%s', delimiter='\t')
+np.savetxt(select_dataset + '/eigen_train_files.txt', np.array(eigen_train), fmt='%s', delimiter='\t')
 eigen_train_file.close()
 
 # ====== #
 #  Test  #
 # ====== #
 # Read Monodepths Files
-eigen_test_file = open("../monodepth/eigen_test_files.txt", "r")
+eigen_test_file = open("../../../modules/third_party/monodepth/utils/filenames/eigen_test_files.txt", "r")
 eigen_test = []
 
 for line in eigen_test_file:
@@ -68,12 +78,14 @@ for line in eigen_test_file:
     # print(splitted[1])
     # print(splitted)
 
-    eigen_test.append([newline, newline.replace('imgs', 'disp1')])
+    eigen_test.append([newline, newline.replace('imgs', disp_folder)])
 
 # Display
 for i, pair in enumerate(eigen_test):
     print(i, pair)
 
 # Save
-np.savetxt('kitti_discrete/eigen_test_files.txt', np.array(eigen_test), fmt='%s', delimiter='\t')
+np.savetxt(select_dataset + '/eigen_test_files.txt', np.array(eigen_test), fmt='%s', delimiter='\t')
 eigen_test_file.close()
+
+print("\nDone.")
